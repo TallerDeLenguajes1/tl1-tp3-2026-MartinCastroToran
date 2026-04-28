ac6c0d4dac38ddde0bb5c0f2858a96758df92937
Martin Castro Toran <martincastrotoran@Martins-MacBook-Pro.local>
2026-04-28 13:52:52 -0300

    Resolucion de funcion buscarNombre en el main

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -8,15 +8,18 @@ char buff[MAX];
 void cargarArreglo(char ** vector, int tamVector);
 void liberarMemoria(char ** vector, int tamVector);
 void mostrarPersonas(char ** vector, int tamVector);
+int buscarNombre(char ** vector, int tamVector,char clave[MAX]);
 
 int main(){
     char ** vector; //Vector puntero de punteros
-    int tamNombre, tamVector = 5;
+    int tamNombre, tamVector = 5, valor;
     vector = malloc(sizeof(char *) * tamVector);
     cargarArreglo(vector, tamVector);
     mostrarPersonas(vector, tamVector);
-    liberarMemoria(vector, tamVector);
+    
+    valor = buscarNombre(vector, tamVector, "martin");
 
+    liberarMemoria(vector, tamVector);
 }
 
 
@@ -43,4 +46,17 @@ void mostrarPersonas(char ** vector, int tamVector){
     for (int i = 0; i < tamVector; i++){ //Itero para solicitar y luego almacenar los 5 nombres
         printf("Nombre numero [%d]: %s \n",(i + 1), vector[i]); 
     }
+}
+
+int buscarNombre(char ** vector, int tamVector,char clave[MAX]){
+    for (int i=0; i < tamVector; i++){
+
+        if (strstr(vector[i], clave) != NULL)
+        {
+            printf("Nombre encontrado en la posicion [%d]: %s \n", (i + 1), vector[i]);
+            return 0;
+        }
+    }
+    printf("Nombre no encontrado");
+    return -1;
 }
